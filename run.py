b005a252c7fd4758ef1046d175ca19bb339c6545
Luke Blaney <git@lukeblaney.co.uk>
2018-12-08 17:40:39 +0000

    automatically set isXmas and isHalloween based on today's date, rather than manually updating them

--- run.py
+++ run.py
@@ -1,10 +1,15 @@
 #! /usr/local/bin/python3
-import requests, json, sys
+import requests, json, sys, datetime
 from func import getWeighting
 if len(sys.argv) < 2:
 	exit("Please specify API url")
 apiurl = sys.argv[1]
 page = 0
+
+today = datetime.date.today()
+isDecember = (today.month == 12)
+isLateOctober = (today.month == 10 and today.day > 25)
+
 while True:
 	page += 1
 	tracks = requests.get(apiurl+"/tracks/?page="+str(page)).json()
@@ -12,7 +17,7 @@ while True:
 		break
 
 	for track in tracks:
-		weighting = getWeighting(track, isXmas = False)
+		weighting = getWeighting(track, isXmas = isDecember, isHalloween = isLateOctober)
 		oldweighting = track['weighting']
 		if (oldweighting != weighting):
 			print(track['url'] + " " + str(oldweighting) + " => "+ str(weighting))
