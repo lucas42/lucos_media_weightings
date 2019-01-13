c126b400a7bb18802979212217d7454ebac1dc49
Luke Blaney <git@lukeblaney.co.uk>
2019-01-13 21:03:28 +0000

    better error handling, particularly when the wrong API URL has been given

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 #! /usr/local/bin/python3
 import requests, json, sys, datetime
 from func import getWeighting
+verbose = False
 if len(sys.argv) < 2:
 	exit("Please specify API url")
 apiurl = sys.argv[1]
@@ -10,6 +11,7 @@ today = datetime.date.today()
 isDecember = (today.month == 12)
 isLateOctober = (today.month == 10 and today.day > 25)
 
+print ("\033[0mChecking media library for weightings which have changed...")
 while True:
 	page += 1
 	tracks = requests.get(apiurl+"/tracks/?page="+str(page)).json()
@@ -20,8 +22,15 @@ while True:
 		weighting = getWeighting(track, isXmas = isDecember, isHalloween = isLateOctober)
 		oldweighting = track['weighting']
 		if (oldweighting != weighting):
-			print(track['url'] + " " + str(oldweighting) + " => "+ str(weighting))
-			print(json.dumps(track, indent=2))
-			requests.put(apiurl+"/tracks/"+str(track['trackid'])+"/weighting", data=str(weighting))
+			print("\033[1mWeighting update: " + track['url'] + " " + str(oldweighting) + " => " + str(weighting)+ "\033[0m")
+			if verbose:
+				print(json.dumps(track, indent=2))
+			result = requests.put(apiurl+"/tracks/"+str(track['trackid'])+"/weighting", data=str(weighting), allow_redirects=False)
+			if result.is_redirect:
+				print ("\033[91m** Error ** Redirect returned by server.  Make sure you're using the latest API URL. \033[0m")
+			elif result.ok:
+				print ("\033[92mWeighting updated to: " +  result.text + "\033[0m")
+			else:
+				print ("\033[91m** Error ** HTTP Status code "+str(result.status_code)+" returned by API: " +  result.text + "\033[0m")
 		else:
 			print(track['url'] + " - still " + str(weighting))
