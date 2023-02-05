d72a8d23ec5f580c33fdce0f0e66cf0e30ed69c2
Luke Blaney <git@lukeblaney.co.uk>
2023-02-05 14:36:17 +0000

    use an iterator for the getAllTracks function, making it much simpler to use

--- src/all-tracks.py
+++ src/all-tracks.py
@@ -2,24 +2,20 @@
 from media_api import getAllTracks, updateWeighting, updateWeightingsTimestamp
 from loganne import loganneRequest
 
+# Record in loganne that the script has started
 loganneRequest({
 	"type":"weightings",
 	"humanReadable": "Calculate weightings for all media tracks",
 })
 print ("\033[0mChecking media library for weightings which have changed...")
 
-page = 0
-while True:
-	page += 1
-	tracks = getAllTracks(page)
-	if len(tracks) == 0:
-		break
+# Iterate through every track in the media API and try updating its weighting
+for track in getAllTracks():
+	try:
+		response = updateWeighting(track)
+		print("\033[92m" + track['url'] + " - "+  response + "\033[0m")
+	except Exception as error:
+		print ("\033[91m** Error ** " + str(error) + "\033[0m")
 
-	for track in tracks:
-		try:
-			response = updateWeighting(track)
-			print("\033[92m" + track['url'] + " - "+  response + "\033[0m")
-		except Exception as error:
-			print ("\033[91m** Error ** " + str(error) + "\033[0m")
-
+# Record the timestamp the script completed for monitoring and debug purposes
 updateWeightingsTimestamp()
--- src/media_api.py
+++ src/media_api.py
@@ -7,8 +7,36 @@ apiurl = os.environ.get("MEDIA_API")
 if (apiurl.endswith("/")):
 	sys.exit("\033[91mDon't include a trailing slash in the API url\033[0m")
 
-def getAllTracks(page):
-	return requests.get(apiurl+"/tracks/?page="+str(page)).json()
+class getAllTracks:
+	"""Returns an iterator covering all tracks in the media API
+
+	Gets tracks one page at a time and returns them track-by-track
+	When out of tracks, fetches the next page and returns those track-by-track
+	Finishes when out of pages
+	"""
+	def __init__(self):
+		self.page = 0
+		self.tracks = []
+
+	def __iter__(self):
+		return self
+
+	def __next__(self):
+
+		# While there's still tracks left in the page of tracks, use the next of those
+		if len(self.tracks) > 0:
+			return self.tracks.pop(0)
+
+		# If there's none left, fetch the next page
+		self.page += 1
+		self.tracks = requests.get(apiurl+"/tracks/?page="+str(self.page)).json()
+
+		if len(self.tracks) > 0:
+			return self.tracks.pop(0)
+
+		# If there's no tracks in the next page, then all tracks have been returned
+		else:
+			raise StopIteration
 
 def updateWeighting(track):
 	verbose = False
