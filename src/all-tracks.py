cce73071156f0201c1d21404300e90a81b2b5c4c
Luke Blaney <git@lukeblaney.co.uk>
2026-02-08 01:58:11 +0000

    feat: Migrate Loganne integration to `lucos-loganne-pythonclient`, removing the custom `loganne.py` module and updating environment variables.

--- src/all-tracks.py
+++ src/all-tracks.py
@@ -1,14 +1,14 @@
 #! /usr/local/bin/python3
 from media_api import getAllTracks, updateWeighting
-from loganne import loganneRequest
+from loganne import updateLoganne
 from schedule_tracker import updateScheduleTracker
 from log_util import info, error
 
 # Record in loganne that the script has started
-loganneRequest({
-	"type":"weightings",
-	"humanReadable": "Calculate weightings for all media tracks",
-})
+updateLoganne(
+	type="weightings",
+	humanReadable="Calculate weightings for all media tracks"
+)
 info("Checking media library for weightings which have changed...")
 
 # Iterate through every track in the media API and try updating its weighting
--- src/loganne.py
+++
@@ -1,8 +0,0 @@
-import requests
-from log_util import error
-
-def loganneRequest(data):
-	data["source"] = "lucos_media_weightings"
-	loganne_reponse = requests.post('https://loganne.l42.eu/events', json=data);
-	if loganne_reponse.status_code != 202:
-		error("Call to Loganne failed with "+str(loganne_reponse.status_code)+" response: " +  loganne_reponse.text)
